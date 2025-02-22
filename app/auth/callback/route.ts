16047eb0eb6266424a3b9ab87f57e2c0dfb46fe5
S-Axhwin <ashwin.sathiyamoorthy.d@gmail.com>
2025-02-23 03:58:56 +0530

    some fixed some pro

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -9,7 +9,7 @@ export async function GET(request: Request) {
     const supabase = await createClient()
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) {
-      const forwardedHost = request.headers.get('x-forwarded-host') // original origin before load balancer
+      const forwardedHost = "https://main.d31uiqamv7wz5n.amplifyapp.com"
       const isLocalEnv = process.env.NODE_ENV === 'development'
       if (isLocalEnv) {
         return NextResponse.redirect(`${origin}${next}`)
